03c2abd25fd96647feaa1f8fc6b4666a3c568e65
Emmy2721 <96811283+Emmy2721@users.noreply.github.com>
2024-01-16 05:39:46 +0000

    newpiglatin

--- PigLatin.js
+++ PigLatin.js
@@ -1,24 +1,22 @@
-function translatePigLatin(str) {
-  
-  if (firstpositon > 0) {
-    return str.slice(firstpositon) + str.slice(0, firstpositon) + "ay";
-    
+function translateWord(word) {
+
+  if (/^[aeiou]/i.test(word)) {
+      return word + 'way';
   }
 
   
-  const lowerInput = input.toLowerCase();
-
-  const isVowel = (char) => ['a', 'e', 'i', 'o', 'u'].includes(char);
-
-
-  const translateWord = (word) => {
-    if (isVowel(word[0])) {
-      return word + 'way';
-    } else if (isVowel(word[1])) {
+  else if (/^[^aeiou]{1}/i.test(word)) {
       return word.slice(1) + word[0] + 'ay';
-    } else {
+  }
+
+ 
+  else if (/^[^aeiou]{2}/i.test(word)) {
       return word.slice(2) + word.slice(0, 2) + 'ay';
-    }
+  }
 
+  return word;
+}
 
-translatePiglatin("consonant");
+console.log(translateWord("Hello")); 
+
+//Hello ,elloHay
--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.body.innerHTML = '<h1> Introduction </h1>';
-
-var FirstName = "Mahlet";
-var lastName = "Birhanu";
-var yearsOfStudy = 2;
-var goal = "Become a web developer";
