b04447f71493d9d2e2ec3ed0aa1c8f763693a87e
Emmy2721 <96811283+Emmy2721@users.noreply.github.com>
2024-01-27 03:53:51 +0000

    new change

---
+++ students.js
@@ -0,0 +1,22 @@
+document.body.innerHTML = '<h1> Srudents Grade </h1>';
+
+
+let grades = "jim|25, sue|32, mary|34, ann|22, ted|28, frank|15, lisa|19, mike|30, ahn|26, vishaya|27";
+grades.split(', ')
+
+  
+(10) [0: 'jim|25', 1: 'sue|32', 2: 'mary|34', 'ann|22', 'ted|28', 'frank|15', 'lisa|19', 'mike|30', 'ahn|26', 'vishaya|27'
+0: "jim|25"
+1: "sue|32"
+2: "mary|34"
+3: "ann|22"
+4: "ted|28"
+5: "frank|15"
+6: "lisa|19"
+7: "mike|30"
+8: "ahn|26"
+9: "vishaya|27"
+length: 10
+[[Prototype]]: Array(0)
+
+
